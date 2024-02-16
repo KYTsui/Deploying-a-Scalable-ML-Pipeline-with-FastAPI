626687a1bbbbeb230515a0ec9e648e16d6cca3ca
ka yi tsui <ktsui@wgu.edu>
2024-02-16 03:05:36 -0500

    Corrected typo

--- ml/model.py
+++ ml/model.py
@@ -71,7 +71,7 @@ def inference(model, X):
 
     Inputs
     ------
-    model : ???
+    model : LogisticRegression()
         Trained machine learning model.
     X : np.array
         Data used for prediction.
